c9419b09a2bab7212e7389910a15a21ad2e6124b
Nadavgo8 <19nadav99@gmail.com>
2025-09-02 14:09:27 +0300

    completed join

--- Very_Hard/join.js
+++ Very_Hard/join.js
@@ -17,9 +17,29 @@ join(["aaa", "bbb", "ccc", "ddd"]) ➞ ["aaabbbcccddd", 0]
 Notes
 More specifically, look at the overlap between the previous words ending letters and the next word's beginning letters.
 */
-
-function join( /*args*/ ) {
-  //your code
+function countOverlaps(a, b) {
+  const maxOverlaps = Math.min(a.length, b.length);
+  for (let i = maxOverlaps; i > 0; i--) {
+    if (a.endsWith(b.slice(0, i))) {
+      return i;
+    }
+  }
+  return 0;
 }
+
+function join(arr) {
+  let merged = arr[0];
+  let minOverlap = Infinity;
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    const k = countOverlaps(arr[i], arr[i + 1]);
+    minOverlap = Math.min(minOverlap, k);
+    merged += arr[i + 1].slice(k);
+  }
+
+  if (minOverlap === Infinity) minOverlap = 0;
+  return [merged, minOverlap];
+}
+console.log(join(["lebron", "ronaldo", "messi"]));
 
 exports.solution = join;
